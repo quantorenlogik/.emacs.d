a3fff32e1402803110114ed31a45de07c39ef7f6
Kai Poblocki <quantorenlogik@gmail.com>
2019-07-30 00:58:39 +0200

    splitting config with org-mode

--- init.el
+++ init.el
@@ -1,10 +1,17 @@
+;;; init.el --- Emacs init file
+;;  Author: Kai Poblocki
+;;; Commentary:
+;;; Personal Emacs config.
+;;; Code:
 
+;; require essential packages
 (require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+;; Setting up package manager and doom-themes
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -13,68 +20,16 @@
   (package-refresh-contents)
   (package-install 'doom-themes))
 
-(setq make-backup-file nil)
-(setq auto-save-default nil)
-
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(defvar my-term-shell "/bin/bash")
-(defadvice ansi-term (before force-bash)
-  (interactive (list my-term-shell)))
-(ad-activate 'ansi-term)
-(global-set-key (kbd "<s-return>") 'ansi-term)
-
-(setq scroll-conservatively 100)
-
-(setq ring-bell-function 'ignore)
-
-(when window-system (global-hl-line-mode t))
-
-(when window-system (global-prettify-symbols-mode t))
-
-(use-package which-key
-  :ensure t
-  :init
-  (which-key-mode))
-
-(use-package beacon
-  :ensure t
-  :init
-  (beacon-mode 1))
-
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-
-(setq inhibit-startup-message t)
-
-(require 'doom-themes)
-
-;; Global settings (defaults)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
-;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
-;; may have their own settings.
-(load-theme 'doom-one t)
-
-;; Enable flashing mode-line on errors
-(doom-themes-visual-bell-config)
-
-;; Enable custom neotree theme (all-the-icons must be installed!)
-(doom-themes-neotree-config)
-;; or for treemacs users
-(doom-themes-treemacs-config)
-
-;; Corrects (and improves) org-mode's native fontification.
-(doom-themes-org-config)
+;; Load main config file "./config.org"
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (beacon doom-themes which-key use-package))))
+ '(package-selected-packages (quote (doom-themes which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
