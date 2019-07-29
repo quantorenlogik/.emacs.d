cfe527513e682e41428abc9a5509f37090849543
Kai Poblocki <quantorenlogik@gmail.com>
2019-07-29 20:17:28 +0200

    further config

--- init.el
+++ init.el
@@ -13,17 +13,34 @@
   (package-refresh-contents)
   (package-install 'doom-themes))
 
+(setq make-backup-file nil)
+(setq auto-save-default nil)
+
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (defvar my-term-shell "/bin/bash")
 (defadvice ansi-term (before force-bash)
   (interactive (list my-term-shell)))
 (ad-activate 'ansi-term)
+(global-set-key (kbd "<s-return>") 'ansi-term)
+
+(setq scroll-conservatively 100)
+
+(setq ring-bell-function 'ignore)
+
+(when window-system (global-hl-line-mode t))
+
+(when window-system (global-prettify-symbols-mode t))
 
 (use-package which-key
   :ensure t
   :init
   (which-key-mode))
+
+(use-package beacon
+  :ensure t
+  :init
+  (beacon-mode 1))
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -57,7 +74,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (doom-themes which-key use-package))))
+ '(package-selected-packages (quote (beacon doom-themes which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
