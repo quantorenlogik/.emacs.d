3775e84b8b4a80333665512b53d600c7a3a8dbce
Kai Poblocki <poblocki.kai@gmail.com>
2019-08-09 15:52:13 +0200

    refinements

--- init.el
+++ init.el
@@ -34,7 +34,8 @@
     ("a94f1a015878c5f00afab321e4fef124b2fc3b823c8ddd89d360d710fc2bddfc" default)))
  '(package-selected-packages
    (quote
-    (centaur-tabs solaire-mode fancy-battery doom-modeline all-the-icons htmlize org-download dmenu powerline company files sudo-edit switch-window avy doom-themes which-key use-package))))
+    (deft centaur-tabs solaire-mode fancy-battery doom-modeline all-the-icons htmlize org-download dmenu powerline company files sudo-edit switch-window avy doom-themes which-key use-package)))
+ '(solaire-mode-remap-fringe t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
