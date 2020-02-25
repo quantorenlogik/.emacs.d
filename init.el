0eedbccaf0b49730599d893a7b41f62d2d3f2b72
Kai Poblocki <poblocki@posteo.de>
2020-02-25 01:09:55 +0100

    complete overhaul and mu4e-integration with gpg

--- init.el
+++ init.el
@@ -1,49 +1,47 @@
 ;;; init.el --- Emacs init file
-;;  Author: Kai Poblocki
-;;; Commentary:
-;;; Personal Emacs config.
+;;; Author: Kai Poblocki
 ;;; Code:
+(defvar file-name-handler-alist-original file-name-handler-alist)
 
-;; require essential packages
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6
+      file-name-handler-alist nil
+      site-run-file nil)
+
+(defvar ian/gc-cons-threshold 20000000)
+
+(add-hook 'emacs-startup-hook ; hook run after loading init files
+          (lambda ()
+            (setq gc-cons-threshold ian/gc-cons-threshold
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist file-name-handler-alist-original)))
+
+(add-hook 'minibuffer-setup-hook (lambda ()
+                                   (setq gc-cons-threshold (* ian/gc-cons-threshold 2))))
+(add-hook 'minibuffer-exit-hook (lambda ()
+                                  (garbage-collect)
+                                  (setq gc-cons-threshold ian/gc-cons-threshold)))
+
 (require 'package)
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-;; Setting up package manager and doom-themes
+;; workaround bug in Emacs 26.2
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+;; Setting up the package manager. Install if missing.
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
-(unless (package-installed-p 'doom-themes)
-  (package-refresh-contents)
-  (package-install 'doom-themes))
+(eval-and-compile
+  (setq use-package-always-ensure t))
 
 ;; Load main config file "./config.org"
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a94f1a015878c5f00afab321e4fef124b2fc3b823c8ddd89d360d710fc2bddfc" default)))
- '(package-selected-packages
-   (quote
-    (biblio openwith auctex-latexmk latex auctex multiple-cursors olivetti writeroom-mode adaptive-wrap deft centaur-tabs solaire-mode fancy-battery doom-modeline all-the-icons htmlize org-download dmenu powerline company files sudo-edit switch-window avy doom-themes which-key use-package)))
- '(solaire-mode-remap-fringe t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 95 :width normal :foundry "outline" :family "DejaVu Sans Mono")))))
-(put 'upcase-region 'disabled nil)
-
 (provide 'init)
-;;; end
+;;; init.el ends here
